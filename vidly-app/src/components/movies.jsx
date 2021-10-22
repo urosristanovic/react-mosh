1963a6e9eea5b910ccf8871b606817c98656fc83
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-22 13:08:45 +0200

    Working with table

---
+++ vidly-app/src/components/movies.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { getMovies, deleteMovie } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = movie => {
+    console.log(movie);
+    // const movies = this.state.movies.filter(m => m._id !== movie._id);
+
+    const movies = deleteMovie(movie._id);
+
+    this.setState({ movies: movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    return (
+      <>
+        <p>Showing {count} movies in the database.</p>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className='btn btn-danger'
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+
+  //   state = {
+  //     movies: getMovies(),
+  //   };
+
+  //   render() {
+  //     console.log(this.state.movies);
+  //     return (
+  //       <table className='table'>
+  //         <thead>
+  //           <tr>
+  //             <th scope='col'>Title</th>
+  //             <th scope='col'>Genre</th>
+  //             <th scope='col'>Number in stock</th>
+  //             <th scope='col'>Daily Rental Rate</th>
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           {this.state.movies.map((movie, index) => {
+  //             return (
+  //               <tr key={index}>
+  //                 <td>{movie.title}</td>
+  //                 <td>{movie.genre.name}</td>
+  //                 <td>{movie.numberInStock}</td>
+  //                 <td>{movie.dailyRentalRate}</td>
+  //                 <button className='btn btn-danger'>Delete</button>
+  //               </tr>
+  //             );
+  //           })}
+  //         </tbody>
+  //       </table>
+  //     );
+  //   }
+}
+
+export default Movies;
