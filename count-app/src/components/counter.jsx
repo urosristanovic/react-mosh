8b73d081f96966765b9cec465c6b366e5e0e5ee7
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-22 11:36:00 +0200

    Initial commit (this keyword)

---
+++ count-app/src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3'],
+  };
+
+  constructor() {
+    super();
+    // console.log('Constructor', this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    console.log('Increment Clicked', this);
+    console.log('Increment Clicked', this.state.count);
+  }
+
+  render() {
+    return (
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
+      </>
+    );
+  }
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+}
+
+export default Counter;
