94a85c42654b6c903588446024e45c9dfe0d0a60
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-27 11:58:52 +0200

    6-Pagination - Paginating Data

--- vidly-app/src/components/movies.jsx
+++ vidly-app/src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { getMovies, deleteMovie } from '../services/fakeMovieService';
 import Pagination from './common/pagination';
 import Like from './common/like';
+import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
   state = {
@@ -29,8 +30,11 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.props;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
       <>
@@ -46,7 +50,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
@@ -72,8 +76,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
-          currentPage={this.state.currentPage}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </>
