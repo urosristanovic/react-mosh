800d1ebc832a61e23c301471e9e208e1faf7f6f6
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-27 19:37:39 +0200

    10 - Filtering - Displaying Items

--- vidly-app/src/components/common/listGroup.jsx
+++ vidly-app/src/components/common/listGroup.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const ListGroup = () => {
-  return null;
+const ListGroup = ({ items, textProperty, valueProperty }) => {
+  return (
+    <ul className='list-group'>
+      {items.map(item => (
+        <li key={item[valueProperty]} className='list-group-item'>
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
--- vidly-app/src/components/movies.jsx
+++ vidly-app/src/components/movies.jsx
@@ -48,10 +48,12 @@ class Movies extends Component {
     const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
-      <div className='row'>
+      <div className='row p-4'>
         <div className='col-3'>
           <ListGroup
             items={this.state.genres}
+            textProperty='name'
+            valueProperty='_id'
             onItemSelect={this.handleGenreSelect}
           />
         </div>
