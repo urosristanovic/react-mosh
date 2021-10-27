28c0e1fbfbb2b2a07feaf79dee7c02917ca676af
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-27 19:45:11 +0200

    12 - Filtering - Handling Selection

--- vidly-app/src/components/common/listGroup.jsx
+++ vidly-app/src/components/common/listGroup.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const ListGroup = ({ items, textProperty, valueProperty }) => {
+const ListGroup = ({
+  items,
+  textProperty,
+  valueProperty,
+  selectedItem,
+  onItemSelect,
+}) => {
   return (
     <ul className='list-group'>
       {items.map(item => (
-        <li key={item[valueProperty]} className='list-group-item'>
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? 'list-group-item active' : 'list-group-item'
+          }
+        >
           {item[textProperty]}
         </li>
       ))}
