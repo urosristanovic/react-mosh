521640a17f4e6769dc8773123485050703a3d645
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-28 11:09:34 +0200

    22 - Sorting - Unique Keys - Final

--- vidly-app/src/components/moviesTable.jsx
+++ vidly-app/src/components/moviesTable.jsx
@@ -29,7 +29,7 @@ class MoviesTable extends Component {
   ];
 
   render() {
-    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
     return (
       <table className='table'>
         <TableHeader
@@ -38,18 +38,6 @@ class MoviesTable extends Component {
           onSort={onSort}
         />
         <TableBody data={movies} columns={this.columns} />
-        {/* <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td></td>
-              <td></td>
-            </tr>
-          ))}
-        </tbody> */}
       </table>
     );
   }
