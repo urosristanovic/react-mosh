b1b0d5ab88607ac747f2fdd0ee82edeb7776e6b1
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-28 13:16:23 +0200

    10 - Query String Parameters

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const { sortBy } = queryString.parse(location.search);
   return (
     <div>
       <h1>Posts</h1>
