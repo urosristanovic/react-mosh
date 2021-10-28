758715447829b05d9b0e23ec684d11414e64a94f
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-28 10:41:49 +0200

    20 - Sorting - Extracting TableBody

--- vidly-app/src/components/moviesTable.jsx
+++ vidly-app/src/components/moviesTable.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Like from './common/like';
+import TableBody from './common/tableBody';
 import TableHeader from './common/tableHeader';
 
 class MoviesTable extends Component {
@@ -21,6 +22,7 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
+        <TableBody data={movies} />
         <tbody>
           {movies.map(movie => (
             <tr key={movie._id}>
