dae3dc4a892997a9723eaa4a7563f818c284b0de
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-29 10:47:22 +0200

    05 - Controlled Elements

--- vidly-app/src/components/loginForm.jsx
+++ vidly-app/src/components/loginForm.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
-  username = React.createRef();
-
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  state = {
+    account: { username: '', password: '' },
+  };
 
   handleSubmit = e => {
     e.preventDefault();
-
-    const username = this.username.current.value;
-    console.log('Submitted', username);
+    console.log('Submitted');
   };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
     return (
       <div>
@@ -21,8 +24,9 @@ class LoginForm extends Component {
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
             <input
+              value={this.state.account.username}
+              onChange={this.handleChange}
               autoFocus
-              ref={this.username}
               id='username'
               type='text'
               className='form-control'
