2e7bd8c5c936e3bd5503cd8342e42126d63a5853
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-29 11:14:17 +0200

    08 - Extracting a Reusable Input

--- vidly-app/src/components/loginForm.jsx
+++ vidly-app/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Input from './common/input';
 
 class LoginForm extends Component {
   state = {
@@ -22,29 +23,18 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form action='' onSubmit={this.handleSubmit}>
-          <div className='form-group'>
-            <label htmlFor='username'>Username</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              autoFocus
-              id='username'
-              name='username'
-              type='text'
-              className='form-control'
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id='password'
-              name='password'
-              type='text'
-              className='form-control'
-            />
-          </div>
+          <Input
+            name='username'
+            value={account.username}
+            label='Username'
+            onChange={this.handleChange}
+          />
+          <Input
+            name='password'
+            value={account.password}
+            label='Password'
+            onChange={this.handleChange}
+          />
           <button className='btn btn-primary'>Login</button>
         </form>
       </div>
