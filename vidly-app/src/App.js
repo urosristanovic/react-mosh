044c73c55c812c77adc81b1606297011fda797ba
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-29 10:27:24 +0200

    02 - Building a Bootstrap Form

--- vidly-app/src/App.js
+++ vidly-app/src/App.js
@@ -6,6 +6,7 @@ import MovieForm from './components/movieForm';
 import Movies from './components/movies';
 import NavBar from './components/navBar';
 import NotFound from './components/notFound';
+import LoginForm from './components/loginForm';
 import Rentals from './components/rentals';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className='container'>
           <Switch>
+            <Route path='/login/' component={LoginForm} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies} />
             <Route path='/customers' component={Customers} />
