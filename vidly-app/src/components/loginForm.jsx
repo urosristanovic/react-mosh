dee2631f7269f0180fd483e60df68497286810c6
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-29 13:38:53 +0200

    19 - Extracting Helper Rendering Methods

--- vidly-app/src/components/loginForm.jsx
+++ vidly-app/src/components/loginForm.jsx
@@ -1,7 +1,6 @@
 import Joi from 'joi-browser';
 import React from 'react';
 import Form from './common/form';
-import Input from './common/input';
 
 class LoginForm extends Form {
   state = {
@@ -19,28 +18,13 @@ class LoginForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form action='' onSubmit={this.handleSubmit}>
-          <Input
-            name='username'
-            value={data.username}
-            label='Username'
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name='password'
-            value={data.password}
-            label='Password'
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className='btn btn-primary'>
-            Login
-          </button>
+          {this.renderInput('username', 'Username')}
+          {this.renderInput('password', 'Password', 'password')}
+          {this.renderButton('Login')}
         </form>
       </div>
     );
