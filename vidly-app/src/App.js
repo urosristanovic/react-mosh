d41e4b87048ae64fc10dd1643a681eb58f9277f3
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-29 10:34:26 +0200

    03 - Handling Form Submission

--- vidly-app/src/App.js
+++ vidly-app/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Customers from './components/customers';
+import LoginForm from './components/loginForm';
 import MovieForm from './components/movieForm';
 import Movies from './components/movies';
 import NavBar from './components/navBar';
 import NotFound from './components/notFound';
-import LoginForm from './components/loginForm';
 import Rentals from './components/rentals';
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
         <NavBar />
         <main className='container'>
           <Switch>
-            <Route path='/login/' component={LoginForm} />
+            <Route path='/login' component={LoginForm} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies} />
             <Route path='/customers' component={Customers} />
