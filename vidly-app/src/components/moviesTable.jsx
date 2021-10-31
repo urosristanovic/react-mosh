d92b226e952e4b58ee911b1ba6147824f259f0d4
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-31 14:09:38 +0100

    Fixed decoration on titles

--- vidly-app/src/components/moviesTable.jsx
+++ vidly-app/src/components/moviesTable.jsx
@@ -8,7 +8,11 @@ class MoviesTable extends Component {
     {
       path: 'title',
       label: 'Title',
-      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>,
+      content: movie => (
+        <Link className='no-underline' to={`/movies/${movie._id}`}>
+          {movie.title}
+        </Link>
+      ),
     },
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
