6fb54582ce93664f60d9fa518f3e9921b860927c
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-01 12:01:26 +0100

    9 - Optimistic vs Pessimistic Updates

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -36,9 +36,18 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-    await axios.delete(apiEndPoint + '/' + post.id);
+    const originalPosts = this.state.posts;
+
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndPoint + '/' + post.id);
+      // throw new Error('');
+    } catch (ex) {
+      alert('Something failed while deleting a post!');
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
