d7ceb8da2d4a47936bb861eb9b852613ebccec6e
urosristanovic <urosristanovic@gmail.com>
2021-11-03 12:09:30 +0100

    09 - Authentication and Authorization (#12)
    * 03 - Submitting the Registration Form
    * 04 - Handling Registration Errors
    * 06 - Submitting the Login Form
    * 07 - Handling Login Errors
    * 08 - Storing the JWT
    * 09 - Logging in the User upon Registration
    * 11 - Getting the Current User
    * 12 - Displaying the Current User on NavBar
    * 13 - Logging out a User
    * 14 - Refactoring

--- vidly-app/src/components/registerForm.jsx
+++ vidly-app/src/components/registerForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
-
+import auth from '../services/authService';
+import * as userService from '../services/userService';
 class RegisterForm extends Form {
   state = {
     data: { username: '', password: '', name: '' },
@@ -14,9 +15,18 @@ class RegisterForm extends Form {
     name: Joi.string().required().label('Name'),
   };
 
-  doSubmit = () => {
-    // Call the server
-    console.log('Submitted');
+  doSubmit = async () => {
+    try {
+      const response = await userService.register(this.state.data);
+      auth.loginWithJwt(response.headers['x-auth-token']);
+      this.props.history.push('/');
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
