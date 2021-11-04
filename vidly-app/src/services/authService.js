7d53b27566e58fb8cbaa8c73c996fbc208b19bf2
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-04 10:08:08 +0100

    16 - Fixing Bi-directional Dependencies

--- vidly-app/src/services/authService.js
+++ vidly-app/src/services/authService.js
@@ -5,6 +5,8 @@ import jwtDecode from 'jwt-decode';
 const apiEndpoint = apiUrl + '/auth';
 const tokenKey = 'token';
 
+http.setJwt(getJwt());
+
 export async function login(email, password) {
   const { data: jwt } = http.post(apiEndpoint, {
     email,
