e42d30a5b4532fbbe8eaba014d9897e334199805
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-04 10:02:23 +0100

    15 - Calling Protected API Endpoints

--- vidly-app/src/services/authService.js
+++ vidly-app/src/services/authService.js
@@ -30,9 +30,14 @@ export function getCurrentUser() {
   }
 }
 
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
+
 export default {
   login,
   logout,
   getCurrentUser,
   loginWithJwt,
+  getJwt,
 };
