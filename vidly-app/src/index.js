76d42ac35fca9b9b1be1e34ddee5631632a7da99
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-04 12:01:38 +0100

    02 - Environment Variables

--- vidly-app/src/index.js
+++ vidly-app/src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
+console.log(process.env);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
