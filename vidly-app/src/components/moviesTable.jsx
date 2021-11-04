21c34ff122a9f08e715d0905eb91901788b30cde
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-04 11:28:52 +0100

    23 - Hiding the Delete Column by Mosh

--- vidly-app/src/components/moviesTable.jsx
+++ vidly-app/src/components/moviesTable.jsx
@@ -20,25 +20,45 @@ class MoviesTable extends Component {
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       ),
     },
-    {
-      key: 'delete',
-      content: movie => {
-        const user = auth.getCurrentUser();
-        if (user && user.isAdmin) {
-          return (
-            <button
-              onClick={() => this.props.onDelete(movie)}
-              className='btn btn-danger btn-sm'
-            >
-              Delete
-            </button>
-          );
-        } else {
-          return null;
-        }
-      },
-    },
+    // {
+    //   key: 'delete',
+    //   content: movie => {
+    //     const user = auth.getCurrentUser();
+    //     if (user && user.isAdmin) {
+    //       return (
+    //         <button
+    //           onClick={() => this.props.onDelete(movie)}
+    //           className='btn btn-danger btn-sm'
+    //         >
+    //           Delete
+    //         </button>
+    //       );
+    //     } else {
+    //       return null;
+    //     }
+    //   },
+    // },
   ];
+
+  deleteColumn = {
+    key: 'delete',
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className='btn btn-danger btn-sm'
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
