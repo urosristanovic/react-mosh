ed3689b1f0f5eef0159f663deb20d88eb790b474
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-04 10:43:29 +0100

    19 - Protecting Routes

--- vidly-app/src/services/authService.js
+++ vidly-app/src/services/authService.js
@@ -1,29 +1,26 @@
+import jwtDecode from 'jwt-decode';
 import http from './httpService';
 import { apiUrl } from '../config.json';
-import jwtDecode from 'jwt-decode';
 
 const apiEndpoint = apiUrl + '/auth';
 const tokenKey = 'token';
 
 http.setJwt(getJwt());
 
-async function login(email, password) {
-  const { data: jwt } = http.post(apiEndpoint, {
-    email,
-    password,
-  });
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
   localStorage.setItem(tokenKey, jwt);
 }
 
-function loginWithJwt(jwt) {
+export function loginWithJwt(jwt) {
   localStorage.setItem(tokenKey, jwt);
 }
 
-function logout() {
+export function logout() {
   localStorage.removeItem(tokenKey);
 }
 
-function getCurrentUser() {
+export function getCurrentUser() {
   try {
     const jwt = localStorage.getItem(tokenKey);
     return jwtDecode(jwt);
@@ -32,14 +29,14 @@ function getCurrentUser() {
   }
 }
 
-function getJwt() {
+export function getJwt() {
   return localStorage.getItem(tokenKey);
 }
 
 export default {
   login,
+  loginWithJwt,
   logout,
   getCurrentUser,
-  loginWithJwt,
   getJwt,
 };
